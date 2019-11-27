45d28f974b83eec4a6d2dd6de0d351e5fb817264
atanasrusenov <atanas@itpx.one>
2019-11-27 14:34:11 +0100

    initial commit

---
+++ kafka-publisher-service/index.js
@@ -0,0 +1,67 @@
+const kafka = require('kafka-node');
+
+const PRODUCER_TOPIC = process.env.PRODUCER_TOPIC || 'fraud-detected';
+const KAFKA_HOST = process.env.KAFKA_HOST || 'localhost:9092';
+
+const client = new kafka.KafkaClient({ kafkaHost: KAFKA_HOST });
+const producer = new kafka.Producer(client);
+
+const COUNTRIES = ['bg', 'gb', 'de', 'nl', 'in', 'jp', 'dk', 'se'];
+
+const getRandomCountry = () => {
+    return COUNTRIES[Math.floor(Math.random() * COUNTRIES.length)];
+}
+
+const getRandomInt = (min, max) => {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+const getRandomCC = () => {
+    const parts = new Array(4).fill(0)
+        .map(() => new Array(4).fill(0)
+                .map(() => getRandomInt(0, 9))
+                .join('')
+        );
+        
+    return parts.join(' ');
+}
+
+producer.on('ready', () => {
+    const readline = require('readline');
+    readline.emitKeypressEvents(process.stdin);
+    process.stdin.setRawMode(true);
+    process.stdin.on('keypress', (str, key) => {
+        if (key.ctrl && key.name === 'c') {
+            process.exit();
+            return;
+        } else if (key && key.name === 'return') {
+            const payload = { country: getRandomCountry(), card: getRandomCC() };;
+            console.log('Preparing to publish payload: ', payload);
+            producer.send([{
+                topic: PRODUCER_TOPIC,
+                messages: [JSON.stringify(payload)],
+                timestamp: Date.now()
+            }], (err, data) => {
+                if (err) {
+                    console.log('Error publishing message:', err);
+                } else {
+                    console.log('Published', data);
+                }
+            });
+        }
+    });
+
+    console.log('Kafka producer ready & waiting for you to press [Enter] to publish events. Press Ctrl + C to exit.');
+});
+
+producer.on('error', (err) => {
+    console.error('Error connecting to kafka:', err);
+});
+
+client.createTopics([{ topic: PRODUCER_TOPIC, partitions: 1, replicationFactor: 1 }], (err, result) => {
+    if (err) {
+        console.warn('Attempted to create topic and failed: ', err);
+    }
+});
---
+++ pdf-service/index.js
@@ -0,0 +1,39 @@
+const { generatePdf } = require('tea-school');
+const path = require('path');
+const express = require('express'),
+    app = express(),
+    bodyParser = require('body-parser');
+
+// support parsing of application/json type post data
+app.use(bodyParser.json());
+
+app.post('/api/generate', (req, res) => {
+    const options = {
+        htmlTemplatePath: path.resolve(__dirname, 'template.pug'),
+
+        // Here you put an object according to https://github.com/sass/node-sass#options 
+        styleOptions: {
+            file: path.resolve(__dirname, 'template.scss')
+        },
+
+        // Here you put an object according to https://pugjs.org/api/reference.html#options
+        // You can add any additional key to be used as a variable in the template.
+        htmlTemplateOptions: req.body,
+
+        // Her  e you put an object according to https://github.com/GoogleChrome/puppeteer/blob/v1.18.1/docs/api.md#pagepdfoptions
+        pdfOptions: {
+            format: 'A4',
+            printBackground: true
+        }
+    }
+
+    generatePdf(options).then((buffer) => {
+        console.log('PDF generated!')
+        res.send({ data: { file: buffer.toString('base64') } });
+    }).catch(err => {
+        console.error('Error generating PDF:', err);
+        res.status(500).send({ err: 'Error generating report' });
+    });
+})
+
+app.listen(4000, () => console.log(`PDF service listing on port ${4000}! 🚀`));
